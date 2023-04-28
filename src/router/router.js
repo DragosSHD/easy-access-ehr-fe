c658cca56634424822a420d3f6b7cd0efb384939
Dragos Dobre <dragos@bigkittylabs.com>
2023-04-28 13:02:32 +0300

    feat: show toast on unauthorized access.

--- src/router/router.js
+++ src/router/router.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import { getUser } from "../util/util.js";
 import routeNames from "./routeNames.js";
+import ToastifyEs from "toastify-js/src/toastify-es.js";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -37,6 +38,18 @@ const router = createRouter({
 router.beforeEach((to) => {
   const user = getUser();
   if (to.name !== routeNames.LOGIN && to.name !== routeNames.REGISTER && !user) {
+    ToastifyEs({
+      text: "You need to sign in first.",
+      duration: 2000,
+      close: true,
+      gravity: "top", // `top` or `bottom`
+      position: "center", // `left`, `center` or `right`
+      stopOnFocus: true, // Prevents dismissing of toast on hover
+      style: {
+        background: "#E88080FF",
+      },
+      onClick: function(){} // Callback after click
+    }).showToast();
     return { name: routeNames.LOGIN }
   }
   if ((to.name === routeNames.LOGIN || to.name === routeNames.REGISTER || to.path === '/') && user) {
